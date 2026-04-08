bb0566fefaae0c022f57195db4ae21ddb8ae1997
riyadh03 <leprofessionnel3.0@gmail.com>
2026-04-08 16:44:50 +0100

    Mise a jour fu frontend : frontend(a utiliser), frontend-old(a ne pas utiliser)

---
+++ frontend/lib/api.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Add interceptor for auth token if needed
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default api
